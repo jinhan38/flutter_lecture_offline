620b37ca26a87bbbf547910e0a754c1290a6792d
jinhan <jinhan38@naver.com>
2023-04-01 12:15:57 +0900

    4주차 TextFormField 작업중

--- lib/ch4/text_form_field_screen.dart
+++ lib/ch4/text_form_field_screen.dart
@@ -13,6 +13,33 @@ class _TextFormFieldScreenState extends State<TextFormFieldScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("TextFormFieldScreen"),
+
+        // true : back 버튼 자동으로 보여줌
+        // false : back 버튼 자동으로 보여주지 않음
+        automaticallyImplyLeading: true,
+      ),
+      body: body(),
+    );
+  }
+
+  Widget body() {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: TextFormField(
+        style: TextStyle(fontSize: 20),
+        cursorColor: Colors.pink,
+        cursorWidth: 5,
+        // cursorHeight: 35,
+        cursorRadius: Radius.circular(3),
+        decoration: InputDecoration(
+          // hint : 글자를 입력하지 않았을 때 보여주는 문구
+          hintText: "아이디를 입력하세요",
+          hintStyle: TextStyle(
+            fontSize: 20,
+            color: Colors.grey,
+          ),
+          hintMaxLines: 1,
+        ),
       ),
     );
   }
