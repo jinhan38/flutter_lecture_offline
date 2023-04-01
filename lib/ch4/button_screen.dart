cd7698c4d9759f1ba6859f7e3aa930f4681c52df
jinhan <jinhan38@naver.com>
2023-04-01 15:35:55 +0900

    4주차 Button 기본 스타일 설정

--- lib/ch4/button_screen.dart
+++ lib/ch4/button_screen.dart
@@ -31,18 +31,63 @@ class _ButtonScreenState extends State<ButtonScreen> {
   }
 
   Widget _elevatedButton() {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    // print('width : $width, height : $height');
     return ElevatedButton(
       // 버튼을 클릭 했을 때 진입
-      onPressed: () {},
+      onPressed: () {
+        print('onPressed');
+      },
 
       // 버튼을 누르고 있을 때 진입
-      onLongPress: () {},
+      onLongPress: () {
+        print('onLongPress');
+      },
 
       // 마우스 커서가 위에 있을 때
       // 웹
       onHover: (value) {
-        print('value : $value');
+        // print('value : $value');
       },
+
+      // 버튼 비활성화 처리
+      // onPressed: null,
+
+      style: ElevatedButton.styleFrom(
+        // 버튼의 컬러
+        backgroundColor: Colors.orange,
+
+        // 텍스트의 컬러와 클릭했을 때 splash 컬러 변경
+        foregroundColor: Colors.black,
+        shadowColor: Colors.purple,
+        elevation: 5,
+
+        // Size 클래스는 2개의 값을 넣을 수 있어서
+        // 첫 번째는 가로(width), 두 번째는 세로(height)
+        // 고정된 사이즈
+        fixedSize: Size(width * 0.8, height * 0.1),
+        // minimumSize: Size(width - 200, 60),
+        // maximumSize: Size(800, 150),
+
+        // 버튼 내부 패딩
+        padding: EdgeInsets.only(bottom: 3),
+
+        // child의 위치 설정
+        alignment: Alignment.center,
+
+        // Button disabled 됐을 때 배경 컬러
+        disabledBackgroundColor: Colors.grey,
+
+        // Button disabled 됐을 텍스트 컬러
+        disabledForegroundColor: Colors.white,
+
+        disabledMouseCursor: SystemMouseCursors.basic,
+
+        enabledMouseCursor:SystemMouseCursors.grab,
+
+      ),
+
       child: Text("ElevatedButton"),
     );
   }
