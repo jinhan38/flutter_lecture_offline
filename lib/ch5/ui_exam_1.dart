9ddbefed229465787bb4195e30bedb8196bb4a4d
jinhan <jinhan38@naver.com>
2023-04-08 17:24:47 +0900

    예제 UI 커밋

--- lib/ch5/ui_exam_1.dart
+++ lib/ch5/ui_exam_1.dart
@@ -8,8 +8,119 @@ class UiExam1 extends StatefulWidget {
 }
 
 class _UiExam1State extends State<UiExam1> {
+  final PageController pageController = PageController(viewportFraction: 0.9);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        title: const Text("복잡한 UI"),
+        centerTitle: true,
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _top(),
+            const SizedBox(height: 20),
+            _middle(),
+            const SizedBox(height: 20),
+            _bottom(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _top() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            iconItem("택시"),
+            iconItem("블랙"),
+            iconItem("바이크"),
+            iconItem("대리"),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            iconItem("택시"),
+            iconItem("블랙"),
+            iconItem("바이크"),
+            const SizedBox(width: 50),
+            // iconItem("대리"),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget iconItem(String text) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.local_taxi,
+              size: 32,
+            )),
+        Text(text),
+      ],
+    );
+  }
+
+  Widget _middle() {
+    return SizedBox(
+      height: 200,
+      child: PageView(
+        controller: pageController,
+        children: [
+          image(),
+          image(),
+          image(),
+        ],
+      ),
+    );
+  }
+
+  Widget image() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(child: Image.asset("assets/winter.jpg")),
+      ),
+    );
+  }
+
+  Widget _bottom() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 60,
+          child: Row(
+            children: const [
+              SizedBox(width: 20),
+              Icon(Icons.notifications_none_sharp),
+              SizedBox(width: 20),
+              Text("[이벤트]이것은 공지사항입니다.")
+            ],
+          ),
+        );
+      },
+      itemCount: 10,
+    );
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:f_lecture/ch4/text_form_field_screen.dart';
 import 'package:f_lecture/ch5/gridview_screen.dart';
 import 'package:f_lecture/ch5/listview_screen.dart';
 import 'package:f_lecture/ch5/pageview_screen.dart';
+import 'package:f_lecture/ch5/ui_exam_1.dart';
 import 'package:flutter/material.dart';
 
 import 'ch3/widgets/column_row_widget.dart';
@@ -66,6 +67,11 @@ class HomeScreen extends StatelessWidget {
               context: context,
               text: "PageViewScreen",
               page: const PageViewScreen(),
+            ),
+            button(
+              context: context,
+              text: "UiExam1",
+              page: const UiExam1(),
             ),
           ],
         ),
