591b299ceb63fe87e860110a7afcc09519f71308
jinhan38 <jinhan38@naver.com>
2023-04-14 22:21:53 +0900

    ui 예제 수정

--- lib/ch5/ui_exam_1.dart
+++ lib/ch5/ui_exam_1.dart
@@ -8,7 +8,13 @@ class UiExam1 extends StatefulWidget {
 }
 
 class _UiExam1State extends State<UiExam1> {
-  final PageController pageController = PageController(viewportFraction: 0.9);
+  final PageController pageController = PageController(viewportFraction: 0.85);
+
+  List<String> imageList = [
+    "assets/winter.jpg",
+    "assets/winter.jpg",
+    "assets/winter.jpg",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +24,9 @@ class _UiExam1State extends State<UiExam1> {
         foregroundColor: Colors.black,
         title: const Text("복잡한 UI"),
         centerTitle: true,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+        ],
         backgroundColor: Colors.white,
         elevation: 0,
       ),
@@ -27,7 +35,9 @@ class _UiExam1State extends State<UiExam1> {
           children: [
             _top(),
             const SizedBox(height: 20),
-            _middle(),
+            _middle((index) {
+              debugPrint("$index 번째 이미지 클릭");
+            }),
             const SizedBox(height: 20),
             _bottom(),
           ],
@@ -37,25 +47,55 @@ class _UiExam1State extends State<UiExam1> {
   }
 
   Widget _top() {
+    // return GridView.count(
+    //   shrinkWrap: true,
+    //    padding: const EdgeInsets.symmetric(horizontal: 20),
+    //    crossAxisCount: 4,
+    //    mainAxisSpacing: 0,
+    //    crossAxisSpacing: 0,
+    //    childAspectRatio: 1,
+    //    children: [
+    //      iconItem("택시"),
+    //      iconItem("블랙"),
+    //      iconItem("바이크"),
+    //      iconItem("대리"),
+    //      iconItem("택시"),
+    //      iconItem("블랙"),
+    //      iconItem("바이크"),
+    //    ],
+    //  );
     return Column(
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            iconItem("택시"),
-            iconItem("블랙"),
-            iconItem("바이크"),
-            iconItem("대리"),
+            iconItem("택시", () {
+              debugPrint('택시 클릭');
+            }),
+            iconItem("블랙", () {
+              debugPrint('블랙 클릭');
+            }),
+            iconItem("바이크", () {
+              debugPrint('바이크 클릭');
+            }),
+            iconItem("대리", () {
+              debugPrint('대리 클릭');
+            }),
           ],
         ),
-        const SizedBox(height: 10),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            iconItem("택시"),
-            iconItem("블랙"),
-            iconItem("바이크"),
-            const SizedBox(width: 50),
+            iconItem("택시", () {
+              debugPrint('택시 클릭');
+            }),
+            iconItem("블랙", () {
+              debugPrint('블랙 클릭');
+            }),
+            iconItem("바이크", () {
+              debugPrint('바이크 클릭');
+            }),
+            const SizedBox(width: 80),
             // iconItem("대리"),
           ],
         )
@@ -63,44 +103,94 @@ class _UiExam1State extends State<UiExam1> {
     );
   }
 
-  Widget iconItem(String text) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        IconButton(
-            onPressed: () {},
-            icon: const Icon(
+  Widget iconItem(String text, Function() onPressed) {
+    return SizedBox(
+      width: 80,
+      height: 80,
+      child: TextButton(
+        onPressed: () => onPressed(),
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          padding: EdgeInsets.zero,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
               Icons.local_taxi,
-              size: 32,
-            )),
-        Text(text),
-      ],
+              size: 40,
+            ),
+            Text(text),
+          ],
+        ),
+      ),
     );
+    // return Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   crossAxisAlignment: CrossAxisAlignment.center,
+    //   children: [
+    //     IconButton(
+    //       onPressed: () {},
+    //       icon: const Icon(
+    //         Icons.local_taxi,
+    //         size: 32,
+    //       ),
+    //     ),
+    //     Text(text),
+    //   ],
+    // );
   }
 
-  Widget _middle() {
+  Widget _middle(Function(int index) onTap) {
     return SizedBox(
       height: 200,
-      child: PageView(
+      child: PageView.builder(
         controller: pageController,
-        children: [
-          image(),
-          image(),
-          image(),
-        ],
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () => onTap(index),
+            child: image(imageList[index]),
+          );
+        },
+        itemCount: imageList.length,
       ),
     );
+
+    // return SizedBox(
+    //   height: 200,
+    //   child: PageView(
+    //     controller: pageController,
+    //     children: [
+    //       List.generate(imageList.length, (index) => null)
+    //     ],
+    //   ),
+    // );
   }
 
-  Widget image() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: Container(child: Image.asset("assets/winter.jpg")),
+  Widget image(String url) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14),
+        image: DecorationImage(
+          image: Image.asset(url, fit: BoxFit.cover).image,
+          fit: BoxFit.cover,
+        ),
       ),
     );
+    // return Padding(
+    //   padding: const EdgeInsets.symmetric(horizontal: 5),
+    //   child: ClipRRect(
+    //     borderRadius: BorderRadius.circular(10),
+    //     child: Image.asset(
+    //       "assets/winter.jpg",
+    //       fit: BoxFit.cover,
+    //     ),
+    //   ),
+    // );
   }
 
   Widget _bottom() {
@@ -108,15 +198,21 @@ class _UiExam1State extends State<UiExam1> {
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       itemBuilder: (context, index) {
-        return SizedBox(
-          height: 60,
-          child: Row(
-            children: const [
-              SizedBox(width: 20),
-              Icon(Icons.notifications_none_sharp),
-              SizedBox(width: 20),
-              Text("[이벤트]이것은 공지사항입니다.")
-            ],
+        return GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: (){
+            debugPrint("알림 목록 $index");
+          },
+          child: SizedBox(
+            height: 60,
+            child: Row(
+              children: const [
+                SizedBox(width: 20),
+                Icon(Icons.notifications_none_sharp),
+                SizedBox(width: 20),
+                Text("[이벤트]이것은 공지사항입니다.")
+              ],
+            ),
           ),
         );
       },
